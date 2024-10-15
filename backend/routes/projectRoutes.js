4f6672ff7d9a952dc28e653f4b019aa3ca4237bd
Nyamisachela <140169644+Nyamisachela@users.noreply.github.com>
2024-10-15 13:07:29 +0300

    Delete backend directory

--- backend/routes/projectRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { getProjects, addProject } = require('../controllers/projectsController');
-const router = express.Router();
-
-router.route('/projects').get(getProjects).post(addProject);
-module.exports = router;
