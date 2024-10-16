44172bed07485334c1bc919e2b9e0d0f0c81ead1
Nyamisachela <140169644+Nyamisachela@users.noreply.github.com>
2024-10-16 11:13:34 +0000

    add buttons

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="mailto:nyamisachelagat@gmail.com"
-                >
+                  href="mailto:nyamisachelagat@gmail.com">
                   Contact Me
                 </a>
               </li>
